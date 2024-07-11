a17517c150c9331eef090e1d096cf8af3d2635a3
Margott23 <margott23@mail.ru>
2024-07-11 15:43:10 +0300

    Methods for calculated time of add() for ArrayList and LinkedList. Number of items in collection is indicated in the console.
    Example of console output:
    Time ArrayList: add in first = 437ms
    Time ArrayList: add in middle = 195ms
    Time ArrayList: add in fine = 7ms
    Time LinkedList: add in first = 8ms
    Time LinkedList: add in middle = 6742ms
    Time LinkedList: add in fine = 6ms

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,77 @@
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getTimeToAddInFirstArrayList(100000));
-        System.out.println(getTimeToAddInMiddleArrayList(100000));
-        System.out.println(getTimeToAddInFineArrayList(100000));
-        System.out.println(getTimeToAddInFirstLinkedList(100000));
-        System.out.println(getTimeToAddInMiddleLinkedList(100000));
-        System.out.println(getTimeToAddInFineLinkedList(100000));
-}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество элементов");
+        int length = sc.nextInt();
+        System.out.println(getTimeToAddInFirstArrayList(length));
+        System.out.println(getTimeToAddInMiddleArrayList(length));
+        System.out.println(getTimeToAddInFineArrayList(length));
+        System.out.println(getTimeToAddInFirstLinkedList(length));
+        System.out.println(getTimeToAddInMiddleLinkedList(length));
+        System.out.println(getTimeToAddInFineLinkedList(length));
+        sc.close();
+    }
 
-    static String getTimeToAddInFineArrayList(int lengthArrayList) {
+    /**
+     * Создает и добавляет в конец коллекции элемент: Math.random() * 1000 количеством раз, переданном в аргументе.
+     * Возвращает строковое представление времени выполнения метода для указанного количества элементов коллекции.
+     *
+     * @param lengthList number of items in the collection
+     * @return String "Time ArrayList: add in fine = time ms", where "time" - method execution time.
+     */
+    static String getTimeToAddInFineArrayList(int lengthList) {
         Date startTime = new Date();
         List<Double> doubleArrayList = new ArrayList<>();
-        for (int i = 0; i < lengthArrayList; i++) {
+        for (int i = 0; i < lengthList; i++) {
             doubleArrayList.add(Math.random() * 1000);
         }
         Date finishTime = new Date();
         return "Time ArrayList: add in fine = " + (finishTime.getTime() - startTime.getTime()) + "ms";
     }
 
-    static String getTimeToAddInFirstArrayList(int lengthArrayList) {
+    /**
+     * Создает и добавляет в начало коллекции элемент: Math.random() * 1000 количеством раз, переданном в аргументе.
+     * Возвращает строковое представление времени выполнения метода для указанного количества элементов коллекции.
+     *
+     * @param lengthList number of items in the collection
+     * @return String "Time ArrayList: add in fine = time ms", where "time" - method execution time.
+     */
+    static String getTimeToAddInFirstArrayList(int lengthList) {
         Date startTime = new Date();
         List<Double> doubleArrayList = new ArrayList<>();
-        for (int i = 0; i < lengthArrayList; i++) {
+        for (int i = 0; i < lengthList; i++) {
             doubleArrayList.add(0, Math.random() * 1000);
         }
         Date finishTime = new Date();
         return "Time ArrayList: add in first = " + (finishTime.getTime() - startTime.getTime()) + "ms";
     }
 
-    static String getTimeToAddInMiddleArrayList(int lengthArrayList) {
+    /**
+     * Создает и добавляет в середину коллекции элемент: Math.random() * 1000 количеством раз, переданном в аргументе.
+     * Возвращает строковое представление времени выполнения метода для указанного количества элементов коллекции.
+     *
+     * @param lengthList number of items in the collection
+     * @return String "Time ArrayList: add in fine = time ms", where "time" - method execution time.
+     */
+    static String getTimeToAddInMiddleArrayList(int lengthList) {
         Date startTime = new Date();
         List<Double> doubleArrayList = new ArrayList<>();
-        for (int i = 0; i < lengthArrayList; i++) {
+        for (int i = 0; i < lengthList; i++) {
             doubleArrayList.add(i / 2, Math.random() * 1000);
         }
         Date finishTime = new Date();
         return "Time ArrayList: add in middle = " + (finishTime.getTime() - startTime.getTime()) + "ms";
     }
 
+    /**
+     * Создает и добавляет в конец коллекции элемент: Math.random() * 1000 количеством раз, переданном в аргументе.
+     * Возвращает строковое представление времени выполнения метода для указанного количества элементов коллекции.
+     *
+     * @param lengthList number of items in the collection
+     * @return String "Time ArrayList: add in fine = time ms", where "time" - method execution time.
+     */
     static String getTimeToAddInFineLinkedList(int lengthList) {
         Date startTime = new Date();
         List<Double> doubleLinkedList = new LinkedList<>();
@@ -53,6 +82,13 @@ public class Main {
         return "Time LinkedList: add in fine = " + (finishTime.getTime() - startTime.getTime()) + "ms";
     }
 
+    /**
+     * Создает и добавляет в начало коллекции элемент: Math.random() * 1000 количеством раз, переданном в аргументе.
+     * Возвращает строковое представление времени выполнения метода для указанного количества элементов коллекции.
+     *
+     * @param lengthList number of items in the collection
+     * @return String "Time ArrayList: add in fine = time ms", where "time" - method execution time.
+     */
     static String getTimeToAddInFirstLinkedList(int lengthList) {
         Date startTime = new Date();
         List<Double> doubleLinkedList = new LinkedList<>();
@@ -63,6 +99,13 @@ public class Main {
         return "Time LinkedList: add in first = " + (finishTime.getTime() - startTime.getTime()) + "ms";
     }
 
+    /**
+     * Создает и добавляет в середину коллекции элемент: Math.random() * 1000 количеством раз, переданном в аргументе.
+     * Возвращает строковое представление времени выполнения метода для указанного количества элементов коллекции.
+     *
+     * @param lengthList number of items in the collection
+     * @return String "Time ArrayList: add in fine = time ms", where "time" - method execution time.
+     */
     static String getTimeToAddInMiddleLinkedList(int lengthList) {
         Date startTime = new Date();
         List<Double> doubleLinkedList = new LinkedList<>();
